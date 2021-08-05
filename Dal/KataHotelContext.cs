b57f1912aa43adebeb867c084e87397381667dea
Kevin Sousselier <knok_proof@hotmail.com>
2021-08-05 21:36:37 +0200

    Remove hard config in context

--- Dal/KataHotelContext.cs
+++ Dal/KataHotelContext.cs
@@ -21,15 +21,6 @@ namespace Dal
         public virtual DbSet<Room> Rooms { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-UL1GRPOS\\SQLEXPRESS;Database=KataHotel;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "French_CI_AS");
